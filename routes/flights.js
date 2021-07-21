68f77ae043ddfd668a298f548fa0b56e742ba2d8
Msamson09 <michaelssamson@gmail.com>
2021-07-20 20:51:50 -0700

    functional

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+import * as flightsCtrl from "../controllers/flights.js"
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
-})
+router.get('/', flightsCtrl.index)
+router.post("/", flightsCtrl.create)
+router.get("/new", flightsCtrl.newFlight)
+router.get("/:id", flightsCtrl.show)
+router.post("/:id/tickets", flightsCtrl.createTicket)
+router.post("/:id/destinations", flightsCtrl.addToDestinations)
 
 export {
   router
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-import('./config/database.js')
-
+import("./config/database.js")
 import { router as indexRouter } from './routes/index.js'
 import { router as flightsRouter } from './routes/flights.js'
+import { router as destinationsRouter } from "./routes/destinations.js"
 
 const app = express()
 
@@ -29,8 +29,9 @@ app.use(
   )
 )
 
-
+app.use('/', indexRouter)
 app.use('/flights', flightsRouter)
+app.use("/destinations", destinationsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
